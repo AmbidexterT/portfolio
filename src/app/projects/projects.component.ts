c350e8d471ef31a4111737002068ca3581f5b181
Stepan <stepan.petrov@planisense.com>
2022-06-30 19:31:50 +0300

    updates

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 
+// @ts-ignore
+import anime from 'animejs/lib/anime.es.js';
+// @ts-ignore
+import Letterize from 'letterizejs';
 
 @Component({
   selector: 'app-projects',
@@ -16,6 +20,34 @@ export class ProjectsComponent implements OnInit {
   ngOnInit(): void {
     this.venobox = $('.venobox');
     this.venobox.venobox();
+    const test = new Letterize({
+      targets: '.animate-me'
+    });
+
+    const animation = anime.timeline({
+      targets: test.listAll,
+      delay: anime.stagger(100, {
+        grid: [test.list[0].length, test.list.length],
+        from: 'center'
+      }),
+      loop: true
+    });
+
+    animation
+      .add({
+        scale: 0.5
+      })
+      .add({
+        letterSpacing: "13px"
+      })
+      .add({
+        scale: 1
+      })
+      .add({
+        letterSpacing: "7px"
+      });
   }
+
+
 
 }
